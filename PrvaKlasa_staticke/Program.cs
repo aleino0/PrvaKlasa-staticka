baf375268bc69bcb7462fac8a2a1ee660ff694e2
aleino0 <113008053+aleino0@users.noreply.github.com>
2022-11-23 09:24:45 +0100

    Add files via upload

---
+++ PrvaKlasa_staticke/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrvaKlasa_staticke
+{
+    class PrvaKlasa
+    {
+        private static int counter;
+
+        public PrvaKlasa()
+        {
+            counter++;
+        }
+
+        public int Counter { get => counter; set => counter = value; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa();
+            PrvaKlasa y = new PrvaKlasa();
+            PrvaKlasa z = new PrvaKlasa();
+
+            Console.ReadKey();
+        }
+    }
+}
